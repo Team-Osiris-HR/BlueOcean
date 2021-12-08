9fadf4fead3d8acab52aefbdaf661c9965cbdc6a
Matthew Seagren <mattseagren@gmail.com>
2021-12-08 15:43:29 -0600

    Add App.jsx component

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Components {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>Home Page</p>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log('we made it');
+
+// render dom
