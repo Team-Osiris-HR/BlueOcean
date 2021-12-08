84054338ab30f9a4d0d8eaee18d92adbfa96d31b
leftyxiv <manny@aledoux.net>
2021-12-08 15:10:38 -0500

    created the models:

--- models/User.js
+++ models/User.js
@@ -63,3 +63,39 @@ userSchema.pre('save', async function (next) {
   next();
 };
 
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
+  // $gt $gte $lt $lte $ne $in $nin
+  next();
+});
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
