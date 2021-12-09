442511293f2610f77cc8d4cc0312dc2792f3a19d
Kim Honrada <kimhonrada21@gmail.com>
2021-12-08 20:35:04 -0500

    Add signup and login component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,36 @@ import React from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Login from './Login.jsx'
+import Signup from './Signup.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      render: "signup"
+    }
 
     this.renderView = this.renderView.bind(this)
   }
 
   renderView() {
-    return (
-      <Container className='app-container'>
-        <Col>
-          <div>
-            <h1>Hello world</h1>
-          </div>
-        </Col>
-      </Container >
-    )
+    if (this.state.render === "login") {
+      return <Login />
+    } else if (this.state.render === "signup") {
+      return <Signup />
+    } else {
+      return <h1>hello world</h1>
+    }
   }
 
   render() {
     return (
-      this.renderView()
+      <Container className='app-container' >
+        <Col>
+          {this.renderView()}
+        </Col>
+      </Container >
     );
   }
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+class Signup extends React.Component {
+
+  render() {
+    return (
+      <Form>
+        <h1>Signup page</h1>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formGridAddress1">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <Form.Label>Address 2</Form.Label>
+          <Form.Control placeholder="Apartment, studio, or floor" />
+        </Form.Group>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>State</Form.Label>
+            <Form.Select defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Row>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    )
+  }
+}
+
+export default Signup;
