b5cb0846aebf9edca72888b212ef869909f90000
leftyxiv <manny@aledoux.net>
2021-12-09 19:33:18 -0500

    added some user controllers and some post routes andstuff

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User.js');
+const Post = require('../models/Post.js');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllUsers = catchAsync(async (req, res) => {
@@ -41,5 +42,22 @@ exports.getMyInfo = catchAsync(async (req, res) => {
   res.status(200).json({
     status: 'success',
     user: req.user,
+  });
+};
+
+exports.addFavorite = catchAsync(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Post not found',
+    })
+  }
+  user.favorites.push(post);
+  user.save();
+  res.status(200).json({
+    status: 'success',
+    user,
   });
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const authController = require('../controllers/authController.js');
+const userController = require('../controllers/userController.js');
+const postController = require('../controllers/postController.js');
+
+route.use(authController.protect);
+
+router.post('/', postController.createPost);
+
+router.patch('/:id/favorite', userController.addFavorite);
+
 
 module.exports = router;
