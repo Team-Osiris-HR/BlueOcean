22203238e0528c205ab733d2e0b57d0d4a635b49
Kim Honrada <kimhonrada21@gmail.com>
2021-12-11 17:17:59 -0500

    Add floating label on signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Accordion from 'react-bootstrap/Accordion'
 import axios from 'axios'
 
@@ -54,29 +55,58 @@ class Signup extends React.Component {
           <h1>Create an account</h1>
           <Form.Group controlId="formGridName"
             className="mb-3">
-            <Form.Control name='name' placeholder="Name" onChange={(e) => this.handleChange(e)} />
+            <FloatingLabel
+              label='name'
+              className='mb-3'
+            >
+              <Form.Control name='name' placeholder="Name" onChange={(e) => this.handleChange(e)} />
+            </FloatingLabel>
           </Form.Group>
 
           <Row className="mb-3" >
             <Form.Group className="mb-3" as={Col} controlId="formGridEmail">
-              <Form.Control type="email" name='email' placeholder=" Email" onChange={(e) => this.handleChange(e)} />
+              <FloatingLabel
+                label='email'
+                className='mb-3'
+              >
+                <Form.Control type="email" name='email' placeholder=" Email" onChange={(e) => this.handleChange(e)} />
+              </FloatingLabel>
             </Form.Group>
 
             <Form.Group className="mb-3" as={Col} controlId="formGridPassword" >
-              <Form.Control type="password" placeholder="Password (min 6 char)" name='password' onChange={(e) => this.handleChange(e)} />
+              <FloatingLabel
+                label='password'
+                className='mb-3'
+              >
+                <Form.Control type="password" placeholder="Password (min 6 char)" name='password' onChange={(e) => this.handleChange(e)} />
+              </FloatingLabel>
             </Form.Group>
-
             <Form.Group as={Col} controlId="formGridConfirmPassword" >
-              <Form.Control type="password" placeholder="Confirm Password" name='passwordConfirm' onChange={(e) => this.handleChange(e)} />
+              <FloatingLabel
+                label='confirm password'
+                className='mb-3'
+              >
+                <Form.Control type="password" placeholder="Confirm Password" name='passwordConfirm' onChange={(e) => this.handleChange(e)} />
+              </FloatingLabel>
             </Form.Group>
           </Row>
 
           <Form.Group className="mb-3" controlId="formGridAddress" >
-            <Form.Control placeholder="Address" name='address' onChange={(e) => this.handleChange(e)} />
+            <FloatingLabel
+              label='address'
+              className='mb-3'
+            >
+              <Form.Control placeholder="Address" name='address' onChange={(e) => this.handleChange(e)} />
+            </FloatingLabel>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formGridPhone" >
-            <Form.Control placeholder="1230001234" name='phone' onChange={(e) => this.handleChange(e)} />
+            <FloatingLabel
+              label='phone'
+              className='mb-3'
+            >
+              <Form.Control placeholder="1230001234" name='phone' onChange={(e) => this.handleChange(e)} />
+            </FloatingLabel>
           </Form.Group>
 
           <Accordion className="mb-3">
@@ -84,11 +114,21 @@ class Signup extends React.Component {
               <Accordion.Header>non-profit organization?</Accordion.Header>
               <Accordion.Body>
                 <Form.Group className="mb-3" controlId="formGridOrgAddress" >
-                  <Form.Control placeholder="Organization Address" name='organizationAddress' onChange={(e) => this.handleChange(e)} />
+                  <FloatingLabel
+                    label='organization address'
+                    className='mb-3'
+                  >
+                    <Form.Control placeholder="Organization Address" name='organizationAddress' onChange={(e) => this.handleChange(e)} />
+                  </FloatingLabel>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formGridOrgPhone" >
-                  <Form.Control placeholder="Organization Phone" name='organizationPhone' onChange={(e) => this.handleChange(e)} />
+                  <FloatingLabel
+                    label='organization phone'
+                    className='mb-3'
+                  >
+                    <Form.Control placeholder="Organization Phone" name='organizationPhone' onChange={(e) => this.handleChange(e)} />
+                  </FloatingLabel>
                 </Form.Group>
 
               </Accordion.Body>
