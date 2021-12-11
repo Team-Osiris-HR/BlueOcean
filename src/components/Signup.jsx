88f2f6b5208672392918f8ef03ce79554dcc3d34
Kim Honrada <kimhonrada21@gmail.com>
2021-12-11 10:59:17 -0500

    Signup form working, setting state

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,42 +6,52 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.state.name)
+  }
 
   render() {
     return (
       <Container>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <h1>Create an account</h1>
-          <Form.Group controlId="formGridName" md>
-            <Form.Label>Name</Form.Label>
-            <Form.Control placeholder="First Name, Last Name" />
+          <Form.Group controlId="formGridName" md
+            className="mb-3">
+            <Form.Control name='name' placeholder="Name" onChange={(e) => this.handleChange(e)} />
           </Form.Group>
 
           <Row className="mb-3" md="true">
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+            <Form.Group className="mb-3" as={Col} controlId="formGridEmail">
+              <Form.Control type="email" name='email address' placeholder="Enter email" onChange={(e) => this.handleChange(e)} />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridPassword" md>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="mb-3" as={Col} controlId="formGridPassword" md>
+              <Form.Control type="password" placeholder="Password" name='password' onChange={(e) => this.handleChange(e)} />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridConfirmPassword" md>
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Confirm Password" />
+              <Form.Control type="password" placeholder="Confirm Password" name='passwordConfirm' onChange={(e) => this.handleChange(e)} />
             </Form.Group>
           </Row>
 
-          <Form.Group controlId="formGridAddress" md>
-            <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
+          <Form.Group className="mb-3" controlId="formGridAddress" md>
+            <Form.Control placeholder="Address" name='address' onChange={(e) => this.handleChange(e)} />
           </Form.Group>
 
-          <Form.Group controlId="formGridPhone" md>
-            <Form.Label>Phone</Form.Label>
-            <Form.Control placeholder="555-555-5555" />
+          <Form.Group className="mb-3" controlId="formGridPhone" md>
+            <Form.Control placeholder="555-555-5555" name='phone' onChange={(e) => this.handleChange(e)} />
           </Form.Group>
 
           <Button variant="primary" type="submit">
