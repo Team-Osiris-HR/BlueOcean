ed672e1ad6dd5060baf922001867e44a409f6d58
Matthew Seagren <mattseagren@gmail.com>
2021-12-11 12:09:03 -0600

    Testing

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
 import axios from 'axios'
 
+
 class Signup extends React.Component {
   constructor(props) {
     super(props)
@@ -22,7 +23,6 @@ class Signup extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value })
@@ -46,38 +46,32 @@ class Signup extends React.Component {
         alert('check all fields')
       });
   }
+
   render() {
     return (
       <Container>
         <Form onSubmit={this.handleSubmit}>
           <h1>Create an account</h1>
-          <Form.Group controlId="formGridName" md
-            className="mb-3">
+          <Form.Group controlId="formGridName" md="hello" className="mb-3">
             <Form.Control name='name' placeholder="Name" onChange={(e) => this.handleChange(e)} />
           </Form.Group>
-
           <Row className="mb-3" md="true">
             <Form.Group className="mb-3" as={Col} controlId="formGridEmail">
               <Form.Control type="email" name='email' placeholder=" Email" onChange={(e) => this.handleChange(e)} />
             </Form.Group>
-
             <Form.Group className="mb-3" as={Col} controlId="formGridPassword" md>
               <Form.Control type="password" placeholder="Password (min 6 char)" name='password' onChange={(e) => this.handleChange(e)} />
             </Form.Group>
-
             <Form.Group as={Col} controlId="formGridConfirmPassword" md>
               <Form.Control type="password" placeholder="Confirm Password" name='passwordConfirm' onChange={(e) => this.handleChange(e)} />
             </Form.Group>
           </Row>
-
           <Form.Group className="mb-3" controlId="formGridAddress" md>
             <Form.Control placeholder="Address" name='address' onChange={(e) => this.handleChange(e)} />
           </Form.Group>
-
           <Form.Group className="mb-3" controlId="formGridPhone" md>
             <Form.Control placeholder="1230001234" name='phone' onChange={(e) => this.handleChange(e)} />
           </Form.Group>
-
           <Button size="lg" variant="primary" type="submit">
             Submit
           </Button>
