c652db25ad872ae656ecca8bcbd883afb63b5cdf
leftyxiv <manny@aledoux.net>
2021-12-13 16:05:29 -0500

    added post id to the return object

--- controllers/chatroomController.js
+++ controllers/chatroomController.js
@@ -38,6 +38,7 @@ exports.getUserChats = catchAsync(async (req, res) => {
     }
     thisObj['title'] = obj[i].product.title;
     thisObj['photos'] = obj[i].product.photos[0] || [];
+    thisObj['postId'] = obj[i].product._id;
     results.push(thisObj);
   }
   res.status(200).json(results);
