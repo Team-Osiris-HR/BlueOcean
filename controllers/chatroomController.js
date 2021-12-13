684f08b9ff2d6db95df423ea3e189a6786f32b0b
leftyxiv <manny@aledoux.net>
2021-12-13 15:57:51 -0500

    added console lig

--- controllers/chatroomController.js
+++ controllers/chatroomController.js
@@ -55,6 +55,7 @@ exports.getOneUserChat = catchAsync(async (req, res) => {
 
 exports.createRoom = catchAsync(async (req, res) => {
   const post = await Post.findById(req.body.postId);
+  console.log(req.body);
   const newRoom = {
     userOne: req.user,
     userTwo: post.user,
