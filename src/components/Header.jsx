c9946b9ea29a2a2dbf2eed9e3b3fa0ef6e879dd8
Kim Honrada <kimhonrada21@gmail.com>
2021-12-13 16:43:47 -0500

    Remove search bar animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,6 +18,9 @@ import axios from 'axios'
 class Header extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      toggleSearch: 'hide-search'
+    }
     this.clickCategory = this.clickCategory.bind(this)
     this.logout = this.logout.bind(this)
   }
@@ -40,17 +43,16 @@ class Header extends React.Component {
   render() {
     return (
       <Navbar bg="light" expand={false}>
-        <Container fluid>
-          <Col>
+        <Container>
+          <Col >
             <Navbar.Toggle aria-controls="offcanvasNavbar" />
           </Col>
-          <Col xs>
-            {/* <span>mikeslist</span> */}
-            <span >Title</span>
+          <Col>
+            <span>mikeslist</span>
           </Col>
           <Col>
             {this.props.render === 'itempage' ? <Button type="button" variant='warning' onClick={() => { this.props.setRenderState('feed') }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
               </svg>
             </Button> : null}
@@ -59,9 +61,10 @@ class Header extends React.Component {
             <Search setSearch={this.props.setSearch} />
           </Col>
           <Col>
-            <Button onClick={() => { this.props.setRenderState('chat') }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16">
+            <Button className="justify-end" alt-text="messages" onClick={() => { this.props.setRenderState('chat') }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-left-text" viewBox="0 0 16 16">
                 <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
               </svg>
             </Button>
           </Col>
@@ -95,3 +98,10 @@ class Header extends React.Component {
 }
 
 export default Header;
+
+/**
+
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+ */
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -15,7 +15,6 @@ const Search = (props) => {
     event.preventDefault();
   };
 
-
   return (
     <Form>
       <Form.Control
