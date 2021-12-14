c613df1aae379830a60f90e459c30b9a8ee57b94
Kim Honrada <kimhonrada21@gmail.com>
2021-12-13 20:57:36 -0500

    Add a css to dontate button

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -94,7 +94,7 @@ class Feed extends React.Component {
           </Container>
         </div>
         <div className="bottom">
-          <Container className="text-center">
+          <Container>
             <Button className='button' variant="primary" size="lg" onClick={this.toggleDonate}>Donate</Button>
           </Container>
           {this.state.showDonate ?
