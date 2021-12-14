26212321653b208c1553c8e5d847bf217805f5d0
Phil Huynh <philiphuynh98@gmail.com>
2021-12-13 19:36:42 -0500

    donation form update

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -3,48 +3,47 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Modal, Form, Button, FloatingLabel, Row, Col, Stack } from 'react-bootstrap';
 
 
-var Donate = ({toggleDonate, currentUser}) => (
+var Donate = ({toggleDonate, currentUser, handleOnChange, handleFileChange}) => (
 
   <Modal show={toggleDonate} onHide={toggleDonate}>
     <Modal.Body>
       <Form>
-        <Form.Group as={Row} className="mb-3" controlId="formPlaintextUser">
-
-          <Form.Label as={Col} column sm="3">
-            Username:
-          </Form.Label>
-          <Col sm="9">
-            <Form.Control readOnly defaultValue={currentUser.name} />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <Form.Label as={Col} column sm="3">
-            Email:
-          </Form.Label>
-          <Col sm="9">
-            <Form.Control readOnly defaultValue={currentUser.email} />
-          </Col>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <FloatingLabel controlId="floatingPickupDelivery" label="Pickup/Delivery">
-            <Form.Select aria-label="Floating label select example">
-              <option value="negotiable">Negotiable</option>
-              <option value="pickup">Pickup Only</option>
-              <option value="delivery">Delivery Only</option>
-            </Form.Select>
+        <Form.Group>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="I am donating a..."
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              placeholder='Item Name'
+              name='itemName'
+              onChange={handleOnChange}
+            />
           </FloatingLabel>
         </Form.Group>
-        <Form.Group className="mb-3">
-          <FloatingLabel controlId="floatingWhoCanSee" label="Item available for ">
-            <Form.Select aria-label="Floating label select example">
-              <option value="pickup">Charities Only</option>
-              <option value="delivery">All Users</option>
-            </Form.Select>
+        <Form.Group>
+          <FloatingLabel
+            controlId="floatingTextArea"
+            label="Description of Donation"
+            className="mb-3"
+          >
+            <Form.Control
+              as='textarea'
+              placeholder='Description'
+              name='description'
+              style={{height: '100px'}}
+              onChange={handleOnChange}
+            />
           </FloatingLabel>
         </Form.Group>
         <Form.Group className="mb-3">
           <FloatingLabel controlId="floatingWhoCanSee" label="Category My Item Best Fits In">
-            <Form.Select aria-label="Floating label select example">
+            <Form.Select
+              aria-label="Floating label select example"
+              name='category'
+              onChange={handleOnChange}
+            >
               <option value="appliances">Appliances</option>
               <option value="clothes">Clothes</option>
               <option value="electronics">Electronics</option>
@@ -57,27 +56,97 @@ var Donate = ({toggleDonate, currentUser}) => (
             </Form.Select>
           </FloatingLabel>
         </Form.Group>
-        <Form.Group>
-          <FloatingLabel
-            controlId="floatingInput"
-            label="I am donating a..."
-            className="mb-3"
-          >
-            <Form.Control type="text" placeholder='Item Name'/>
+        <Form.Group className="mb-3">
+          <FloatingLabel controlId="floatingCondition" label="Item Condition">
+            <Form.Select
+              aria-label="Floating label select example"
+              name='condition'
+              onChange={handleOnChange}
+            >
+              <option value="new">New</option>
+              <option value="good">Good</option>
+              <option value="fair">Fair</option>
+              <option value="used">Used</option>
+              <option value="poor">Poor</option>
+            </Form.Select>
           </FloatingLabel>
         </Form.Group>
-        <Form.Group>
-          <FloatingLabel
-            controlId="floatingTextArea"
-            label="Description of Donation"
-            className="mb-3"
-          >
-            <Form.Control as='textarea' placeholder='Description' style={{height: '100px'}}/>
+        <Form.Group className="mb-3">
+          <FloatingLabel controlId="floatingPickupDelivery" label="Pickup/Delivery">
+            <Form.Select
+              aria-label="Floating label select example"
+              name='pickupOrDelivery'
+              onChange={handleOnChange}
+            >
+              <option value="negotiable">Negotiable</option>
+              <option value="pickup">Pickup Only</option>
+              <option value="delivery">Delivery Only</option>
+            </Form.Select>
           </FloatingLabel>
         </Form.Group>
-        <Form.Group controlId="photos" className="mb-3">
+        <Form.Group className="mb-3">
+          <FloatingLabel controlId="floatingWhoCanSee" label="Item available for ">
+            <Form.Select
+              aria-label="Floating label select example"
+              name='charitiesOnly'
+              onChange={handleOnChange}
+            >
+              <option value={true}>Charities Only</option>
+              <option value={false}>All Users</option>
+            </Form.Select>
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group controlId="photosLinks" className="mb-3">
+        <Form.Label>Link Photos</Form.Label>
+          <FloatingLabel controlId="photo1" label="Link a Photo">
+            <Form.Control
+              type="text"
+              name="photo1"
+              placeholder="photo1"
+              onChange={handleOnChange}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="photo2" label="Link a Photo">
+            <Form.Control
+              type="text"
+              name="photo2"
+              placeholder="photo2"
+              onChange={handleOnChange}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="photo3" label="Link a Photo">
+            <Form.Control
+              type="text"
+              name="photo3"
+              placeholder="photo3"
+              onChange={handleOnChange}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="photo4" label="Link a Photo">
+            <Form.Control
+              type="text"
+              name="photo4"
+              placeholder="photo4"
+              onChange={handleOnChange}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="photo5" label="Link a Photo">
+            <Form.Control
+              type="text"
+              name="photo5"
+              placeholder="photo5"
+              onChange={handleOnChange}
+            />
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group controlId="photosUpload" className="mb-3">
           <Form.Label>Upload Photos</Form.Label>
-          <Form.Control type="file" multiple />
+          <Form.Control
+            type="file"
+            multiple
+            name="files"
+            onChange={handleFileChange}
+          />
         </Form.Group>
         <Row>
           <Col></Col>
@@ -103,3 +172,4 @@ var Donate = ({toggleDonate, currentUser}) => (
 )
 
 export default Donate;
+
