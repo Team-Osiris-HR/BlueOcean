a2c3ffaad17e6f6b2dbc839071edeb1844f7e6bd
Kim Honrada <kimhonrada21@gmail.com>
2021-12-14 12:21:07 -0500

    Add initial forgot password

---
+++ src/components/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ForgotPassword extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <h1>Forgot Password</h1>
+  }
+}
+
+export default ForgotPassword;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,7 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Alert from 'react-bootstrap/Alert'
 import axios from 'axios'
 import Cookies from 'js-cookie'
+import ForgotPassword from './ForgotPassword.jsx'
 
 class Login extends React.Component {
   constructor(props) {
@@ -12,10 +13,11 @@ class Login extends React.Component {
     this.state = {
       name: '',
       password: '',
-      error: false
+      render: 'login'
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.renderForgotPassword = this.renderForgotPassword.bind(this)
   }
 
 
@@ -33,46 +35,59 @@ class Login extends React.Component {
         this.props.setCurrentUser(result.data.data.user)
         this.props.setRenderState("feed",)
       }).catch((err) => {
-
+        console.log("🚀 ~ file: Login.jsx ~ line 38 ~ Login ~ .then ~ err", err)
       });
   }
 
-  render() {
-    return (
-      <Form className='text-center' onSubmit={this.handleSubmit}>
-        <div className='jumbotron'>
-          <h1 style={{ fontSize: '128px' }}>Hello</h1>
-        </div>
-        <Form.Group className="mb-3 mw-50" controlId="formBasicEmail">
-          <FloatingLabel
-            label='name'
-            className='mb-3'
-          >
-            <Form.Control type="text" name="name" placeholder="name" onChange={(e) => this.handleChange(e)} />
-          </FloatingLabel>
-        </Form.Group>
+  renderForgotPassword() {
+    if (this.state.render === 'login') {
+      this.setState({ render: 'forgot' })
+    } else {
+      this.setState({ render: 'login' })
+    }
+  }
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <FloatingLabel
-            label='password'
-            className='mb-3'
-          >
-            <Form.Control type="password" placeholder="Password" name="password" onChange={(e) => this.handleChange(e)} />
-          </FloatingLabel>
-        </Form.Group>
-        <div className='text-center'>
-          <Button className='button' size="lg" type="submit">
-            Submit
-          </Button>
-        </div>
-        <div>
-          <button className='forgot-password' type="button">forgot password?</button>
-        </div>
-        <div>
-          <button className="create-acc-btn" type="button" onClick={() => this.props.setRenderState('signup')}>don't have an account? click here.</button>
-        </div>
-      </Form >
-    )
+  render() {
+    if (this.state.render === 'login') {
+      return (
+        <Form className='text-center' onSubmit={this.handleSubmit}>
+          <div className='jumbotron'>
+            <h1 style={{ fontSize: '128px' }}>Hello</h1>
+          </div>
+          <Form.Group className="mb-3 mw-50" controlId="formBasicEmail">
+            <FloatingLabel
+              label='name'
+              className='mb-3'
+            >
+              <Form.Control type="text" name="name" placeholder="name" onChange={(e) => this.handleChange(e)} />
+            </FloatingLabel>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <FloatingLabel
+              label='password'
+              className='mb-3'
+            >
+              <Form.Control type="password" placeholder="Password" name="password" onChange={(e) => this.handleChange(e)} />
+            </FloatingLabel>
+          </Form.Group>
+          <div className='text-center'>
+            <Button className='button' size="lg" type="submit">
+              Submit
+            </Button>
+          </div>
+          <div>
+            <button className='forgot-password' type="button" onClick={() => this.renderForgotPassword()}>forgot password?</button>
+          </div>
+          <div>
+            <button className="create-acc-btn" type="button" onClick={() => this.props.setRenderState('signup')}>don't have an account? click here.</button>
+          </div>
+        </Form >
+      )
+    } else {
+      return <ForgotPassword />
+    }
+
   }
 }
 
