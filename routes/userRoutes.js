f8905a5424219bba340e13bfd4ed7f660281fb4b
Alex Carlman <alexcarlman1@gmail.com>
2021-12-15 11:22:08 -0500

    temporarily lift authControl on getAllUsers
    userRoutes.js 19:
    rm second arg authController.restrictTo("admin")

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,7 +16,7 @@ router.get('/setlogoutstatus', userController.setLogOut);
 
 router.get("/logout", authController.logout);
 
-router.get("/", authController.restrictTo("admin"), userController.getAllUsers);
+router.get("/", userController.getAllUsers);
 
 router.get("/myinfo", userController.getMyInfo);
 router.get("/:id", authController.restrictTo("admin"), userController.getUser);
