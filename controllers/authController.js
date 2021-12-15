c7ea3a78527aa56fd4c88cf51c25fde375005890
Kim Honrada <kimhonrada21@gmail.com>
2021-12-15 12:51:05 -0500

    Reset password functioning

--- controllers/authController.js
+++ controllers/authController.js
@@ -148,7 +148,6 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
-  console.log(req.params);
   const hashedToken = crypto
     .createHash("sha256")
     .update(req.params.token)
