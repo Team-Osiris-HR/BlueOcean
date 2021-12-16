6253da053119aa516b377e7e7b87e6cbece16588
mtdupuie <mtdupuie@gmail.com>
2021-12-16 16:27:39 -0500

    fixed a commit

--- src/components/chat/Conversation.jsx
+++ src/components/chat/Conversation.jsx
@@ -8,22 +8,25 @@ import MessageEntry from './MessageEntry.jsx'
 const Conversation = (props) => {
   var messageObj = { message: props.message, time: Date.now(), chatroom: props.chat.chatroomId, name: props.user }
   return (
-    <div>
-      <Row>
-        <Col>{props.chat.title}</Col>
-      </Row>
-      {props.messages.map((message, i) => {
-        return (
-          <MessageEntry
-            key={i}
-            index={i}
-            messages={message}
-            user={props.user} />
-        )
-      })}
-      <div style={{ float: 'right' }}>
-        <input placeholder='type message' value={props.message} onChange={props.handleMessage} ></input>
-        <button onClick={() => { props.sendMessage(props.chat.chatroomId) }}>Send</button>
+    <div className='message-table'>
+      <div className='message-container'>
+        {props.messages.map((message, i) => {
+          return (
+            <MessageEntry
+              key={i}
+              index={i}
+              messages={message}
+              user={props.chat.name} />
+          )
+        })}
+      </div>
+      <div className='user-input'>
+        <div className="message-input-container">
+          <input className='message-input' placeholder='type message' value={props.message} onChange={props.handleMessage} ></input>
+        </div>
+        <div className='send-button-container'>
+          <button className='send-button' onClick={() => { props.sendMessage(props.chat.chatroomId, messageObj) }}>Send</button>
+        </div>
       </div>
     </div>
   )
