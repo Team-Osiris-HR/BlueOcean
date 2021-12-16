0ee759a2bb3f1e4b0491c7b75a8e78102d9d54ba
Kim Honrada <kimhonrada21@gmail.com>
2021-12-16 12:36:48 -0500

    Add a chat header with back button

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -60,64 +60,10 @@ class ChatHeader extends React.Component {
     return (
       <Navbar className='header-container' bg="light" expand={false}>
         <Container fluid>
-          <Col className="p-2 flex-shrink-1">
-            <Navbar.Toggle bg="white" aria-controls="offcanvasNavbar" />
-          </Col>
-          {this.props.render === 'itempage' || this.props.render === 'donoritempage' || this.props.render === 'chat' || this.props.render === 'account' ? <button className='search-button' type="button" variant='warning' onClick={() => { this.props.setRenderState('feed') }}>
-            <BsArrowLeft size={24} />
-          </button> : null}
+          <Button>Back</Button>
           <Col className="p-2 flex-fill">
+            <span>Inbox</span>
           </Col>
-          <Col className="d-flex justify-content-end" xs>
-            {this.props.render === 'feed' ?
-              <button
-                className='search-button' alt-text="messages" onClick={() => { this.props.setRenderState('chat') }}>
-                <BsChatLeftText size={24} />
-              </button>
-              : null}
-          </Col>
-          <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-            placement="start"
-          >
-            <Offcanvas.Header closeButton>
-              {/* <Offcanvas.Title id="offcanvasNavbarLabel">mikeslist</Offcanvas.Title> */}
-              <Offcanvas.Title id="offcanvasNavbarLabel">sample app</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Button variant="primary" className='mb-3' onClick={() => this.props.setRenderState('account')}>my account</Button>
-                <h6>pickup: {this.state.chosenPickup}</h6>
-                <ButtonGroup className="mb-3" aria-label="pickupOption">
-                  <Button variant="primary" onClick={(e) => this.choosePickup(e)}>pickup</Button>
-                  <Button variant="secondary" onClick={(e) => this.choosePickup(e)}>delivery</Button>
-                  <Button variant="primary" onClick={(e) => this.choosePickup(e)}>negotiable</Button>
-                </ButtonGroup>
-                <h6>sort: {this.state.chosenSort}</h6>
-                <ButtonGroup className="mb-3" aria-label="sort">
-                  <Button variant="primary" onClick={(e) => this.chooseSort(e)}>date</Button>
-                  <Button variant="secondary" onClick={(e) => this.chooseSort(e)}>distance</Button>
-                </ButtonGroup>
-                <h6>category: {this.state.chosenCategory}</h6>
-                <ListGroup as="ul" className='mb-5'>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>all</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>appliances</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>clothes</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>electronics</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>food</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>furniture</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>pets</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>toys</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>vehicles</ListGroup.Item>
-                  <ListGroup.Item as="li" onClick={(e) => this.chooseCategory(e)} action>other</ListGroup.Item>
-                </ListGroup>
-              </Nav>
-              <Button onClick={() => { this.logout() }}>
-                logout
-              </Button>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
         </Container>
       </Navbar >
     )
