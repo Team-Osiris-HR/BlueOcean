530783cfcf84d185a7184921a76e5414816eeffe
Kim Honrada <kimhonrada21@gmail.com>
2021-12-16 10:53:39 -0500

    Add chat initial styling

--- src/components/chat/Conversation.jsx
+++ src/components/chat/Conversation.jsx
@@ -6,26 +6,28 @@ import Container from 'react-bootstrap/Container'
 import MessageEntry from './MessageEntry.jsx'
 
 const Conversation = (props) => {
-  var messageObj = {message: props.message, time: Date.now(), chatroom: props.chat.chatroomId, name: props.user}
+  var messageObj = { message: props.message, time: Date.now(), chatroom: props.chat.chatroomId, name: props.user }
   return (
-    <div>
+    <>
       <Row>
         <Col>{props.chat.title}</Col>
       </Row>
-      {props.messages.map((message, i) => {
-        return (
-          <MessageEntry
-            key={i}
-            index={i}
-            messages={message}
-            user={props.chat.name} />
-        )
-      })}
+      <div className='message-container'>
+        {props.messages.map((message, i) => {
+          return (
+            <MessageEntry
+              key={i}
+              index={i}
+              messages={message}
+              user={props.chat.name} />
+          )
+        })}
+      </div>
       <div style={{ float: 'right' }}>
         <input placeholder='type message' value={props.message} onChange={props.handleMessage} ></input>
         <button onClick={() => { props.sendMessage(props.chat.chatroomId, messageObj) }}>Send</button>
       </div>
-    </div>
+    </>
   )
 }
 
