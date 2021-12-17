98972f58d5a864230ab652e877f738780964e5a1
mtdupuie <mtdupuie@gmail.com>
2021-12-17 11:23:49 -0500

    Commit to pull

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -60,7 +60,7 @@ const Map = (props) => {
     axios.get('/api/users/')
       .then ((results) => {
         if (isSubscribed) {
-          var charities = results.data.data.filter(user => user.role==="charity")
+          var charities = results.data.doc.filter(user => user.role==="charity")
           setCharities(charities);
         }
       })
