67e79a789d4f7d7902f3ebce5262fa3b3ba8cf07
Kim Honrada <kimhonrada21@gmail.com>
2021-12-17 11:27:51 -0500

    Add if in postCOntroller

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,8 +14,10 @@ exports.createPost = catchAsync(async (req, res, next) => {
   req.body.user = req.user;
   req.body.username = req.user.name;
   req.body.email = req.user.email;
-  req.body.photos = ["public/img/posts/" + req.file.filename];
-  req.body.photos = req.body.photos.concat(req.body.photoUrls.split(","));
+  if (req.file) {
+    req.body.photos = ["public/img/posts/" + req.file.filename];
+    req.body.photos = req.body.photos.concat(req.body.photoUrls.split(","));
+  }
 
   const newPost = await Post.create(req.body);
   res.status(201).json({
